31cd67f218d4a7d777315cb2a3681716e6c6359f
yehua <1104753531@qq.com>
2019-02-13 09:43:06 +0800

    env

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => Env::get('database.hostname'),
     // 数据库名
     'database'        => Env::get('database.database'),
     // 用户名
