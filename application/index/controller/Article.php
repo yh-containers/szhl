0a2480174f1893b4735ac345c08ddd74918699b5
yehua <1104753531@qq.com>
2019-02-15 18:25:43 +0800

    add

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -6,16 +6,34 @@ class Article extends Common
     //列表
     public function index()
     {
-        if($this->request->isAjax()){
-            $model = new \app\common\model\Article();
-            $data = $model->paginate();
-            return ['code'=>0,'msg'=>'获取成功','data'=>$data];
-        }
-
         return view('index',[
 
         ]);
     }
+
+
+    /*
+     * 产品列表
+     * */
+    public function showList()
+    {
+        $model = new \app\common\model\Article();
+        $arr=[];
+        $list = $model->paginate()->each(function(&$item, $key)use(&$arr){
+            $item= [
+                'id' => $item['id'],
+                'title' => $item['title'],
+                'img' => $item['img'],
+                'send_time' => $item['send_time'],
+                'view' => $item['view'],
+            ];
+            return $item;
+        });
+
+        return ['code'=>1,'msg'=>'获取成功','data'=>$list];
+    }
+
+
     /*
      * 详情
      * */
