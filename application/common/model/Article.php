587c5bfdaeb545b3550785659de422b954a0d812
笕 <1752874948@qq.com>
2019-02-15 16:09:37 +0800

    文章

--- application/common/model/Article.php
+++ application/common/model/Article.php
@@ -10,19 +10,5 @@ class Article extends Base
     protected $name='article';
     public static $fields_status_article=['异常','已发布','取消发布','删除'];
 
-    public function articleDelete($id)
-    {
-
-        $model = $this->where('id',$id)->find($id);
-        $model->delete();
-        if($model){
-
-        }else{
-
-        }
-        dump($model);
-//        $status = $this->where('id',$id)->delete();
-
-    }
 
 }
